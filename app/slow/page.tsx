0385064dfe8efc4c3b2f216a2e3a69ff2b4dc257
nickmurphy-steame <nick.murphy@steame.com>
2025-05-28 09:11:02 -0500

    promise.all

--- app/slow/page.tsx
+++ app/slow/page.tsx
@@ -8,8 +8,10 @@ import { SecondaryContent } from "@/components/SecondaryContent";
 
 export default async function SlowPage() {
   await new Promise((resolve) => setTimeout(resolve, PAGE_DELAY));
-  const primaryCount = await getPrimaryContentCount();
-  const secondaryCount = await getSecondaryContentCount();
+  const [primaryCount, secondaryCount] = await Promise.all([
+    getPrimaryContentCount(),
+    getSecondaryContentCount(),
+  ]);
 
   return (
     <div className="w-full max-w-screen-lg mx-auto grid grid-cols-3 gap-3 items-start">
